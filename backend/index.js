bda3edce7993c7c8973cbc456be0eb816d9978c0
blin4504 <bl4504@g.rit.edu>
2023-02-25 14:10:06 -0500

    connect server and mongodb

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,13 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 const PORT = 3001
 
+mongoose.set('strictQuery', false);
 
+mongoose.connect('mongodb+srv://aw2755:brickhack9@cluster0.rhh3sru.mongodb.net/?retryWrites=true&w=majority')
+.then(() => console.log('connection successful'))
+.catch(err => console.log(err))
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
