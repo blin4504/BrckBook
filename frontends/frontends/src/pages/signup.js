8392cfccc995f9ff0e94a7d206b7f0ce31286486
Alan Wang <aw2755@rit.edu>
2023-02-26 00:01:05 -0500

    lil bit of css

--- frontends/frontends/src/pages/signup.js
+++ frontends/frontends/src/pages/signup.js
@@ -12,52 +12,54 @@ export default function SignUp() {
 
     return (
         <>
-            <div>SignUp</div>
-            <form>
-                <input 
-                type="text" 
-                placeholder="First Name" 
-                value={firstName} onChange={(e) => setName(e.target.value)}
-                />
-            </form>
-            <form>
-                <input 
-                type="text" 
-                placeholder="Last Name"
-                value={lastName} onChange={(e) => setLastName(e.target.value)}
-                />
-            </form>
-            <form>
-                <input 
-                type="text" 
-                placeholder="Email"
-                value={email} onChange={(e) => setEmail(e.target.value)}
-                />
-            </form>
-            <form>
-                <input 
-                type="text" 
-                placeholder="Username"
-                value={userName} onChange={(e) => setUserName(e.target.value)}
-                />
-            </form>
-            <form>
-                <input 
-                type="text" 
-                placeholder="Password"
-                value={password} onChange={(e) => setPassword(e.target.value)}
-                />
-            </form>
-            <form>
-                <input 
-                type="text" 
-                placeholder="Confirm Password"
-                value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)}
-                />
-            </form>
-            <button onClick={() => signClick(firstName, lastName, userName, email, password, confirmPassword)}>Signup</button>
-            <div>
-                <Link to ="login">Already have a account? Login Here</Link>
+            <div class="block">
+                <div>SignUp</div>
+                <form>
+                    <input class="input"
+                    type="text" 
+                    placeholder="First Name" 
+                    value={firstName} onChange={(e) => setName(e.target.value)}
+                    />
+                </form>
+                <form>
+                    <input class="input"
+                    type="text" 
+                    placeholder="Last Name"
+                    value={lastName} onChange={(e) => setLastName(e.target.value)}
+                    />
+                </form>
+                <form>
+                    <input class="input"
+                    type="text" 
+                    placeholder="Email"
+                    value={email} onChange={(e) => setEmail(e.target.value)}
+                    />
+                </form>
+                <form>
+                    <input class="input"
+                    type="text" 
+                    placeholder="Username"
+                    value={userName} onChange={(e) => setUserName(e.target.value)}
+                    />
+                </form>
+                <form>
+                    <input class="input"
+                    type="text" 
+                    placeholder="Password"
+                    value={password} onChange={(e) => setPassword(e.target.value)}
+                    />
+                </form>
+                <form>
+                    <input class="input"
+                    type="text" 
+                    placeholder="Confirm Password"
+                    value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)}
+                    />
+                </form>
+                <button onClick={() => signClick(firstName, lastName, userName, email, password, confirmPassword)}>Signup</button>
+                <div>
+                    <Link to ="login">Already have a account? Login Here</Link>
+                </div>
             </div>
         </>
     )
