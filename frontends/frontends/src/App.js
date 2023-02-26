4b8f120e2e3d3eb5ebad66aecf8937230f8b5e1c
Alan Wang <aw2755@rit.edu>
2023-02-25 22:20:23 -0500

    pls

--- frontends/frontends/src/App.js
+++ frontends/frontends/src/App.js
@@ -1,19 +1,11 @@
 import './App.css';
-import BaseLogin from './Components/BaseLogin';
-import Landing from './Components/Landing';
-import ProfileIcon from './Components/ProfileIcon';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Genre from './Components/Genre';
 
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<BaseLogin/>} />
-        <Route path="/profile-pick" element={<ProfileIcon />} />
-        <Route path="/genre" element={<Genre/>} />
-        <Route path="/landing" element={<Landing/> } />
       </Routes>
 
     </BrowserRouter>
---
+++ frontends/frontends/src/pages/signup.js
@@ -0,0 +1,13 @@
+export default function SignUp(){
+    return(
+        <div>
+            <h1>SignUp</h1>
+            <form>
+                <label>Enter your name:
+                    <input type="text" />
+                </label>
+            </form>
+        </div>
+        
+    );
+}
