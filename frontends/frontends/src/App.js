8607ffc71cfc9218c4413e34cc12813d96a690f4
Alan Wang <aw2755@rit.edu>
2023-02-25 22:15:28 -0500

    added routing

--- frontends/frontends/src/App.js
+++ frontends/frontends/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import BaseLogin from './Components/BaseLogin';
+import Landing from './Components/Landing';
+import ProfileIcon from './Components/ProfileIcon';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Genre from './Components/Genre';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<BaseLogin/>} />
+        <Route path="/profile-pick" element={<ProfileIcon />} />
+        <Route path="/genre" element={<Genre/>} />
+        <Route path="/landing" element={<Landing/> } />
+      </Routes>
+
+    </BrowserRouter>
   );
 }
 
